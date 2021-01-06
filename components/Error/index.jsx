adcc4c4fccbdec12b50c7723fd3e70a1286df6f8
unknown <cadicossamm@gmail.com>
2021-01-06 00:46:00 -0500

    diseño login

---
+++ components/Error/index.jsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+const Error = ({message}) => {
+    return ( 
+        <div className="mt-5 bg-primary p-2 rounded text-center font-semibold text-white text-lg">
+            {message}
+        </div>
+     );
+}
+ 
+export default Error;
--- pages/Login/index.jsx
+++ pages/Login/index.jsx
@@ -1,44 +1,83 @@
+import React, { useState } from 'react';
 import Head from 'next/head';
-import { colors } from "../../styles/theme";
+import Error from "../../components/Error"
 
 
 const Login = () => {
+
+    const [info, setInfo] = useState({
+        username: "",
+        password: ""
+    })
+
+    const [error, setError] = useState(false)
+
+    const { username, password } = info;
+
+    const handleChange = e => {
+        setInfo({
+            ...info,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        //validamos los datos
+        if (username.trim() === "" || password.trim() === "") {
+            setError(true);
+            return;
+        }
+        setError(false);
+
+
+    }
+
+
     return (
         <>
             <main className="container h-screen flex justify-center items-center">
                 <div className="h-1/2 w-3/4 p-2">
 
-                    <div className="text-center ">
-                        <label className="text-2xl font-bold">Login</label>
-                    </div>
-                    <br />
-                    <div className="grid grid-rows-3 gap-5">
-                        <div>
-                            <label htmlFor="username" class="block text-lg font-medium ">Username</label>
-                            <input type="text" name="username" id="username" autocomplete="given-name" class="" />
+                    <form onSubmit={handleSubmit}>
+                        <div className="text-center ">
+                            <label className="text-2xl font-bold">Login</label>
                         </div>
 
-                        <div>
-                            <label htmlFor="password" class="block text-lg font-medium ">Password</label>
-                            <input type="password" name="password" id="password" autocomplete="given-name" class="" />
-                        </div>
+                        {error ? <Error message="Todos los campos son requeridos" /> : null}
 
-                        <div>
-                            <button type="submit" className="">Ingresar</button>
+                        <div className="mt-5 grid grid-rows-3 gap-5">
+                            <div>
+                                <label htmlFor="username" className="block text-lg font-medium ">Username</label>
+                                <input
+                                    type="text"
+                                    name="username"
+                                    id="username"
+                                    onChange={handleChange}
+                                    value={username}
+                                />
+                            </div>
+
+                            <div>
+                                <label htmlFor="password" className="block text-lg font-medium ">Password</label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    onChange={handleChange}
+                                    value={password}
+                                />
+                            </div>
+
+                            <div>
+                                <button type="submit">Ingresar</button>
+                            </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
 
             </main>
-
-            <style jsx>{`
-
-                
-
-            `}</style>
-
-
-
         </>
     );
 }
